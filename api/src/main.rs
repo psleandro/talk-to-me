531437484a9bd67c8c117b369a8f871d2af15852
Leandro Santos <lsaantos@outlook.com>
2025-12-07 10:57:43 -0300

    feat: setup axum api

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, net::SocketAddr};
+use axum::{Router, routing::get};
+use dotenvy::dotenv;
+use tokio::net::TcpListener;
+
+async fn root() -> String {
+    return String::from("Hello world!");
 }
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let app = Router::new().route("/", get(root));
+
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "8000".to_string())
+        .parse()
+        .expect("Failed to parse PORT!");
+
+    let socket_addr = SocketAddr::from(([127,0,0,1], port));
+    println!("App listening on {}", socket_addr);
+
+    let listener = TcpListener::bind(socket_addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
